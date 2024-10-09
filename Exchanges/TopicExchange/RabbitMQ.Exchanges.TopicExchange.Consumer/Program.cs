4d8c91b9e3f38074b7426bb51a5d3f435dcc9248
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-10 01:22:30 +0300

    Using topic exchange in RabbitMQ

---
+++ Exchanges/TopicExchange/RabbitMQ.Exchanges.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+Console.Write("Dinlenecek topic formatını giriniz: ");
+string topic = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example",
+    routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
+
+
---
+++ Exchanges/TopicExchange/RabbitMQ.Exchanges.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+int i = 0;
+while (true)
+{
+    byte[] bytemessage = Encoding.UTF8.GetBytes($"Topic Exchange {++i}");
+    Console.Write("Mesajın gönderileceği topic formatını giriniz: ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: bytemessage);
+}
