3af060e65c492e0d3f2ba913f5a83d55d74494dc
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-10 00:03:57 +0300

    Simple messaging with RabbitMQ

--- SimpleMessaging/RabbitMQ.SimpleMessaging.Consumer/Program.cs
+++ SimpleMessaging/RabbitMQ.SimpleMessaging.Consumer/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
--- SimpleMessaging/RabbitMQ.SimpleMessaging.Publisher/Program.cs
+++ SimpleMessaging/RabbitMQ.SimpleMessaging.Publisher/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+byte[] bytemessage = Encoding.UTF8.GetBytes("SimpleMessaging");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: bytemessage);
+
+Console.Read();
