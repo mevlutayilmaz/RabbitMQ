756ac1e750731e931a0c102c22a3ef8ba5d410b0
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-12 00:25:29 +0300

    Using MassTransit.RabbitMQ in console application.

---
+++ MassTransit/Console/RabbitMQ.MassTransit.Console.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using RabbitMQ.MassTransit.Console.Consumer.Consumers;
+
+string rabbitMQUri = "amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi";
+
+string queueName = "example-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(queueName, enpoint =>
+    {
+        enpoint.Consumer<MessageConsumer>();
+    });
+});
+
+bus.StartAsync();
+
+Console.Read();
---
+++ MassTransit/Console/RabbitMQ.MassTransit.Console.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using RabbitMQ.MassTransit.Shared.Messages;
+
+string rabbitMQUri = "amqps://aczdvobi:WJs-zmyj6qwBqf4FAMoFzrZfQ2wEwt0t@toad.rmq.cloudamqp.com/aczdvobi";
+
+string queueName = "example-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+
+Console.Write("Göderilecek mesaj: ");
+string message = Console.ReadLine();
+
+await sendEndpoint.Send<IMessage>(new Message
+{
+    Text = message
+});
+
+Console.Read();
+
